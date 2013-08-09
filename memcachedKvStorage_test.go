7af628a673bc536a73288f0e94a80b4f16a3f78f
fanngyuan <fanngyuan@gmail.com>
2013-08-09 15:53:32 +0800

    init

---
+++ memcachedKvStorage_test.go
@@ -0,0 +1,72 @@
+package storage
+
+import (
+	"testing"
+	"reflect"
+	"strconv"
+)
+
+type T struct{
+	A int
+}
+
+func TestGetSet(t *testing.T) {
+	tt:=T{1}
+	mcStorage:=NewMcStorage([]string{"localhost:12000"},"test",0,reflect.TypeOf(&tt))
+	mcStorage.Set("1",tt)
+	res,_:=mcStorage.Get("1")
+	defer mcStorage.Delete("1")
+	if reflect.TypeOf(res)!=reflect.TypeOf(&tt){
+		t.Error("res type is not T")
+	}
+	ttRes:=res.(*T)
+	if ttRes.A!=tt.A{
+		t.Error("res A field is not equals tt field")
+	}
+}
+
+func TestMultiGetSet(t *testing.T) {
+	tt:=T{1}
+	mcStorage:=NewMcStorage([]string{"localhost:12000"},"test",0,reflect.TypeOf(&tt))
+	valueMap:=make(map [interface{}] interface{})
+	keys:=make([]interface{},10)
+	for i:=0;i<10;i++{
+		keys[i]=strconv.Itoa(i)
+		valueMap[strconv.Itoa(i)]=T{i}
+		defer mcStorage.Delete(strconv.Itoa(i))
+	}
+	mcStorage.MultiSet(valueMap)
+	res,_:=mcStorage.MultiGet(keys)
+	for k,v :=range(res){
+		if reflect.TypeOf(v)!=reflect.TypeOf(&tt){
+			t.Error("res type is not T")
+		}
+		kint,err:=strconv.Atoi(k.(string))
+		if err!=nil{
+			t.Error("key %s is not int ",k)
+		}
+		vT:=v.(*T)
+		if kint!=vT.A{
+			t.Error("value should be %s,while it is %s",kint,vT.A)
+		}
+	}
+}
+
+func TestGetSetDelete(t *testing.T) {
+	tt:=T{1}
+	mcStorage:=NewMcStorage([]string{"localhost:12000"},"test",0,reflect.TypeOf(&tt))
+	mcStorage.Set("1",tt)
+	res,_:=mcStorage.Get("1")
+	if reflect.TypeOf(res)!=reflect.TypeOf(&tt){
+		t.Error("res type is not T")
+	}
+	ttRes:=res.(*T)
+	if ttRes.A!=tt.A{
+		t.Error("res A field is not equals tt field")
+	}
+	mcStorage.Delete("1")
+	res,_=mcStorage.Get("1")
+	if res!=nil{
+		t.Error("res should be nil ,after delete")
+	}
+}
---
+++ objectStorage.go
@@ -0,0 +1,103 @@
+package storage
+
+type Storage interface{
+	Get(key interface{})(interface{},error)
+	Set(key interface{},object interface{})(error)
+	MultiGet(keys []interface{})(map [interface{}] interface{},error)
+	MultiSet(map [interface{}] interface{})(error)
+	Delete(key interface{})(error)
+}
+
+type StorageProxy struct{
+	PreferedStorage Storage
+	BackupStorage Storage
+}
+
+func (this *StorageProxy) Get(key interface{})(interface{},error){
+	object,err:=this.PreferedStorage.Get(key)
+	if err!=nil{
+		return nil,err
+	}
+	if object==nil{
+		object,err=this.BackupStorage.Get(key)
+		if err!=nil{
+			return nil,err
+		}
+		if object!=nil{
+			this.PreferedStorage.Set(key,object)
+		}
+	}
+	return object,nil
+}
+
+func (this *StorageProxy) Set(key interface{},object interface{})(error){
+	if object!=nil{
+		err:=this.PreferedStorage.Set(key,object)
+		if err!=nil{
+			return err
+		}
+		err=this.BackupStorage.Set(key,object)
+		if err!=nil{
+			return err
+		}
+	}
+	return nil
+}
+
+func (this *StorageProxy) MultiGet(keys []interface{})(map [interface{}] interface{},error){
+	resultMap,err:=this.PreferedStorage.MultiGet(keys)
+	if err!=nil{
+		return nil,err
+	}
+	missedKeyCount:=0
+	for _, key := range keys{
+		_,find:=resultMap[key]
+		if !find{
+			missedKeyCount++
+		}
+	}
+	if missedKeyCount>0{
+		missedKeys:=make([]interface{}, missedKeyCount)
+		i:=0
+		for _, key := range keys{
+			_,find:=resultMap[key]
+			if !find{
+				missedKeys[i]=key
+				i++
+			}
+		}
+		missedMap,err:=this.BackupStorage.MultiGet(missedKeys)
+		if err!=nil{
+			return nil,err
+		}
+		this.MultiSet(missedMap)
+		for k, v := range missedMap {
+			resultMap[k]=v
+		}
+	}
+	return resultMap,nil
+}
+
+func (this *StorageProxy) MultiSet(objectMap map [interface{}] interface{})(error){
+	err:=this.PreferedStorage.MultiSet(objectMap)
+	if err!=nil{
+		return err
+	}
+	err=this.BackupStorage.MultiSet(objectMap)
+	if err!=nil{
+		return err
+	}
+	return nil
+}
+
+func (this *StorageProxy) Delete(key interface{})(error){
+	err:=this.BackupStorage.Delete(key)
+	if err!=nil{
+		return err
+	}
+	err=this.PreferedStorage.Delete(key)
+	if err!=nil{
+		return err
+	}
+	return nil
+}
