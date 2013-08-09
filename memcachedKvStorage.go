963c8656cb74b9d1232d3b08c99097d8e115c869
Tony.Shao <xiocode@gmail.com>
2013-08-09 16:00:50 +0800

    go fmt

--- memcachedKvStorage.go
+++ memcachedKvStorage.go
@@ -1,142 +1,142 @@
 package storage
 
 import (
-	"github.com/bradfitz/gomemcache/memcache"
+	"bytes"
 	"encoding/json"
+	"errors"
+	"github.com/bradfitz/gomemcache/memcache"
 	"reflect"
 	"strings"
-	"errors"
-	"bytes"
 )
 
 type MemcachedKvStorage struct {
-	client *memcache.Client
-	KeyPrefix string
+	client            *memcache.Client
+	KeyPrefix         string
 	DefaultExpireTime int
-	T reflect.Type
+	T                 reflect.Type
 }
 
-func NewMcStorage(serverUrls []string,keyPrefix string,defaultExpireTime int,t reflect.Type)(*MemcachedKvStorage){
+func NewMcStorage(serverUrls []string, keyPrefix string, defaultExpireTime int, t reflect.Type) *MemcachedKvStorage {
 	client := memcache.New(serverUrls...)
-	return &MemcachedKvStorage{client,keyPrefix,defaultExpireTime,t}
+	return &MemcachedKvStorage{client, keyPrefix, defaultExpireTime, t}
 }
 
-func (this *MemcachedKvStorage) Get(key interface{})(interface{},error){
-	cacheKey,err:=BuildCacheKey(this.KeyPrefix,key)
-	if err!=nil{
-		return nil,err
+func (this *MemcachedKvStorage) Get(key interface{}) (interface{}, error) {
+	cacheKey, err := BuildCacheKey(this.KeyPrefix, key)
+	if err != nil {
+		return nil, err
 	}
-	item,err:=this.client.Get(cacheKey)
-	if err!=nil{
-		if err.Error()=="memcache: cache miss"{
-			return nil,nil
+	item, err := this.client.Get(cacheKey)
+	if err != nil {
+		if err.Error() == "memcache: cache miss" {
+			return nil, nil
 		}
-		return nil,err
-	}else{
-		error,object:=this.bytesToInterface(item)
-		if error!=nil{
-			return nil,error
-		}else{
-			return object,nil
+		return nil, err
+	} else {
+		error, object := this.bytesToInterface(item)
+		if error != nil {
+			return nil, error
+		} else {
+			return object, nil
 		}
 	}
 }
 
-func (this *MemcachedKvStorage) bytesToInterface(item *memcache.Item)(error,interface{}){
-	tStruct:=reflect.New(this.T)
+func (this *MemcachedKvStorage) bytesToInterface(item *memcache.Item) (error, interface{}) {
+	tStruct := reflect.New(this.T)
 	dec := json.NewDecoder(bytes.NewBuffer(item.Value))
-	err:=dec.Decode(tStruct.Interface())
-	if err!=nil{
-		return err,nil
+	err := dec.Decode(tStruct.Interface())
+	if err != nil {
+		return err, nil
 	}
-	return nil,tStruct.Elem().Interface()
+	return nil, tStruct.Elem().Interface()
 }
 
-func (this *MemcachedKvStorage)	Set(key interface{},object interface{})(error){
+func (this *MemcachedKvStorage) Set(key interface{}, object interface{}) error {
 	buf, err := json.Marshal(object)
-	if err!=nil{
+	if err != nil {
 		return err
 	}
-	keyCache,err:=BuildCacheKey(this.KeyPrefix,key)
-	if err!=nil{
+	keyCache, err := BuildCacheKey(this.KeyPrefix, key)
+	if err != nil {
 		return err
 	}
-	this.client.Set(&memcache.Item{Key:keyCache , Value:buf})
+	this.client.Set(&memcache.Item{Key: keyCache, Value: buf})
 	return nil
 }
 
-func (this *MemcachedKvStorage)	MultiGet(keys []interface{})(map [interface{}] interface{}, error){
-	cacheKeys:=make([]string,len(keys))
-	for index,key :=range keys{
-		cacheKey,err:=BuildCacheKey(this.KeyPrefix,key)
-		cacheKeys[index]=cacheKey
-		if err!=nil{
-			return nil,err
+func (this *MemcachedKvStorage) MultiGet(keys []interface{}) (map[interface{}]interface{}, error) {
+	cacheKeys := make([]string, len(keys))
+	for index, key := range keys {
+		cacheKey, err := BuildCacheKey(this.KeyPrefix, key)
+		cacheKeys[index] = cacheKey
+		if err != nil {
+			return nil, err
 		}
 	}
-	itemMap,err:=this.client.GetMulti(cacheKeys)
-	if err!=nil{
-		return nil,err
+	itemMap, err := this.client.GetMulti(cacheKeys)
+	if err != nil {
+		return nil, err
 	}
-	result:=make(map [interface{}] interface{})
-	for k,item :=range itemMap{
-		error,object:=this.bytesToInterface(item)
-		if error!=nil{
+	result := make(map[interface{}]interface{})
+	for k, item := range itemMap {
+		error, object := this.bytesToInterface(item)
+		if error != nil {
 			continue
 		}
-		result[GetRawKey(k)]=object
+		result[GetRawKey(k)] = object
 	}
-	return result,nil
+	return result, nil
 }
 
-func (this *MemcachedKvStorage)	MultiSet(objectMap map [interface{}] interface{})(error){
-	for k,v := range objectMap{
-		err:=this.Set(k,v)
-		if err!=nil{
+func (this *MemcachedKvStorage) MultiSet(objectMap map[interface{}]interface{}) error {
+	for k, v := range objectMap {
+		err := this.Set(k, v)
+		if err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func (this *MemcachedKvStorage)	Delete(key interface{})(error){
-	cacheKey,err:=BuildCacheKey(this.KeyPrefix,key)
-	if err!=nil{
+func (this *MemcachedKvStorage) Delete(key interface{}) error {
+	cacheKey, err := BuildCacheKey(this.KeyPrefix, key)
+	if err != nil {
 		return err
 	}
 	this.client.Delete(cacheKey)
 	return nil
 }
 
-func BuildCacheKey(keyPrefix interface{},key interface{})(cacheKey string,err error){
-	if key==nil{
-		return "",errors.New("key should not be nil")
+func BuildCacheKey(keyPrefix interface{}, key interface{}) (cacheKey string, err error) {
+	if key == nil {
+		return "", errors.New("key should not be nil")
 	}
-	if reflect.TypeOf(key)!=reflect.TypeOf("1"){
-		return "",errors.New("key should be string")
+	if reflect.TypeOf(key) != reflect.TypeOf("1") {
+		return "", errors.New("key should be string")
 	}
-	return strings.Join([]string{keyPrefix.(string),key.(string)},"_"),nil
+	return strings.Join([]string{keyPrefix.(string), key.(string)}, "_"), nil
 }
 
-func GetRawKey(key string)(rawKey string){
-	keys:=strings.Split(key,"_")
+func GetRawKey(key string) (rawKey string) {
+	keys := strings.Split(key, "_")
 	return keys[len(keys)-1]
 }
 
 func InitializeStruct(t reflect.Type, v reflect.Value) {
-    for i := 0; i < v.NumField(); i++ {
-        f := v.Field(i)
-        ft := t.Field(i)
-        switch(ft.Type.Kind()){
-        case reflect.Map:
-            f.Set(reflect.MakeMap(ft.Type))
-        case reflect.Slice:
-            f.Set(reflect.MakeSlice(ft.Type, 0, 0))
-        case reflect.Chan:
-            f.Set(reflect.MakeChan(ft.Type, 0))
-        case reflect.Struct:
-            InitializeStruct(ft.Type, f)
-        default:
-        }
-    }
+	for i := 0; i < v.NumField(); i++ {
+		f := v.Field(i)
+		ft := t.Field(i)
+		switch ft.Type.Kind() {
+		case reflect.Map:
+			f.Set(reflect.MakeMap(ft.Type))
+		case reflect.Slice:
+			f.Set(reflect.MakeSlice(ft.Type, 0, 0))
+		case reflect.Chan:
+			f.Set(reflect.MakeChan(ft.Type, 0))
+		case reflect.Struct:
+			InitializeStruct(ft.Type, f)
+		default:
+		}
+	}
 }
--- objectStorage.go
+++ objectStorage.go
@@ -1,102 +1,102 @@
 package storage
 
-type Storage interface{
-	Get(key interface{})(interface{},error)
-	Set(key interface{},object interface{})(error)
-	MultiGet(keys []interface{})(map [interface{}] interface{},error)
-	MultiSet(map [interface{}] interface{})(error)
-	Delete(key interface{})(error)
+type Storage interface {
+	Get(key interface{}) (interface{}, error)
+	Set(key interface{}, object interface{}) error
+	MultiGet(keys []interface{}) (map[interface{}]interface{}, error)
+	MultiSet(map[interface{}]interface{}) error
+	Delete(key interface{}) error
 }
 
-type StorageProxy struct{
+type StorageProxy struct {
 	PreferedStorage Storage
-	BackupStorage Storage
+	BackupStorage   Storage
 }
 
-func (this *StorageProxy) Get(key interface{})(interface{},error){
-	object,err:=this.PreferedStorage.Get(key)
-	if err!=nil{
-		return nil,err
+func (this *StorageProxy) Get(key interface{}) (interface{}, error) {
+	object, err := this.PreferedStorage.Get(key)
+	if err != nil {
+		return nil, err
 	}
-	if object==nil{
-		object,err=this.BackupStorage.Get(key)
-		if err!=nil{
-			return nil,err
+	if object == nil {
+		object, err = this.BackupStorage.Get(key)
+		if err != nil {
+			return nil, err
 		}
-		if object!=nil{
-			this.PreferedStorage.Set(key,object)
+		if object != nil {
+			this.PreferedStorage.Set(key, object)
 		}
 	}
-	return object,nil
+	return object, nil
 }
 
-func (this *StorageProxy) Set(key interface{},object interface{})(error){
-	if object!=nil{
-		err:=this.PreferedStorage.Set(key,object)
-		if err!=nil{
+func (this *StorageProxy) Set(key interface{}, object interface{}) error {
+	if object != nil {
+		err := this.PreferedStorage.Set(key, object)
+		if err != nil {
 			return err
 		}
-		err=this.BackupStorage.Set(key,object)
-		if err!=nil{
+		err = this.BackupStorage.Set(key, object)
+		if err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func (this *StorageProxy) MultiGet(keys []interface{})(map [interface{}] interface{},error){
-	resultMap,err:=this.PreferedStorage.MultiGet(keys)
-	if err!=nil{
-		return nil,err
+func (this *StorageProxy) MultiGet(keys []interface{}) (map[interface{}]interface{}, error) {
+	resultMap, err := this.PreferedStorage.MultiGet(keys)
+	if err != nil {
+		return nil, err
 	}
-	missedKeyCount:=0
-	for _, key := range keys{
-		_,find:=resultMap[key]
-		if !find{
+	missedKeyCount := 0
+	for _, key := range keys {
+		_, find := resultMap[key]
+		if !find {
 			missedKeyCount++
 		}
 	}
-	if missedKeyCount>0{
-		missedKeys:=make([]interface{}, missedKeyCount)
-		i:=0
-		for _, key := range keys{
-			_,find:=resultMap[key]
-			if !find{
-				missedKeys[i]=key
+	if missedKeyCount > 0 {
+		missedKeys := make([]interface{}, missedKeyCount)
+		i := 0
+		for _, key := range keys {
+			_, find := resultMap[key]
+			if !find {
+				missedKeys[i] = key
 				i++
 			}
 		}
-		missedMap,err:=this.BackupStorage.MultiGet(missedKeys)
-		if err!=nil{
-			return nil,err
+		missedMap, err := this.BackupStorage.MultiGet(missedKeys)
+		if err != nil {
+			return nil, err
 		}
 		this.MultiSet(missedMap)
 		for k, v := range missedMap {
-			resultMap[k]=v
+			resultMap[k] = v
 		}
 	}
-	return resultMap,nil
+	return resultMap, nil
 }
 
-func (this *StorageProxy) MultiSet(objectMap map [interface{}] interface{})(error){
-	err:=this.PreferedStorage.MultiSet(objectMap)
-	if err!=nil{
+func (this *StorageProxy) MultiSet(objectMap map[interface{}]interface{}) error {
+	err := this.PreferedStorage.MultiSet(objectMap)
+	if err != nil {
 		return err
 	}
-	err=this.BackupStorage.MultiSet(objectMap)
-	if err!=nil{
+	err = this.BackupStorage.MultiSet(objectMap)
+	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (this *StorageProxy) Delete(key interface{})(error){
-	err:=this.BackupStorage.Delete(key)
-	if err!=nil{
+func (this *StorageProxy) Delete(key interface{}) error {
+	err := this.BackupStorage.Delete(key)
+	if err != nil {
 		return err
 	}
-	err=this.PreferedStorage.Delete(key)
-	if err!=nil{
+	err = this.PreferedStorage.Delete(key)
+	if err != nil {
 		return err
 	}
 	return nil
