dd9013a991a54d13442285ca2edbbb6fc520c4a6
fanngyuan <fanngyuan@gmail.com>
2014-03-10 16:32:28 +0800

    add redis

---
+++ redis.go
@@ -0,0 +1,133 @@
+package storage
+
+import (
+	"github.com/garyburd/redigo/redis"
+	"time"
+)
+
+type RedisClient struct {
+	pool     *redis.Pool
+	addr     string
+}
+
+func (rc *RedisClient) Exists(key string) bool {
+	conn:=rc.connectInit()
+	defer conn.Close()
+	v, err := redis.Bool(conn.Do("EXISTS", key))
+	if err != nil {
+		return false
+	}
+	return v
+}
+
+func (rc *RedisClient) Lpush(key string,value interface{}) error {
+	conn:=rc.connectInit()
+	defer conn.Close()
+
+	_, err := conn.Do("LPUSH", key, value)
+	return err
+}
+
+func (rc *RedisClient) Brpop(key string,timeoutSecs int) (interface{},error) {
+	conn:=rc.connectInit()
+	defer conn.Close()
+
+	var val interface{}
+	var err error
+	if timeoutSecs<0{
+		val, err = conn.Do("BRPOP", key,0)
+	}else{
+		val, err = conn.Do("BRPOP", key,timeoutSecs)
+	}
+	values,err:=redis.Values(val,err)
+	if err!=nil{
+		return nil,err
+	}
+	return string(values[1].([]byte)),err
+}
+
+func (rc *RedisClient) Set(key string,value []byte) error {
+	conn:=rc.connectInit()
+	defer conn.Close()
+
+	_, err := conn.Do("SET", key,value)
+	return err
+}
+
+func (rc *RedisClient) Get(key string) ([]byte,error) {
+	conn:=rc.connectInit()
+	defer conn.Close()
+
+	v, err := conn.Do("GET", key)
+	return v.([]byte),err
+}
+
+func (rc *RedisClient) Incr(key string) error {
+	conn:=rc.connectInit()
+	defer conn.Close()
+
+	_, err := conn.Do("INCR", key)
+	return err
+}
+
+func (rc *RedisClient) Decr(key string) error {
+	conn:=rc.connectInit()
+	defer conn.Close()
+
+	_, err := conn.Do("DECR", key)
+	return err
+}
+
+func (rc *RedisClient) MultiGet(keys []interface{})([]interface{},error){
+	conn:=rc.connectInit()
+	defer conn.Close()
+
+	v, err := conn.Do("MGET", keys...)
+	return v.([]interface{}),err
+}
+
+func (rc *RedisClient) MultiSet(kvMap map[string][]byte) error {
+	conn:=rc.connectInit()
+	defer conn.Close()
+
+	var values []interface{}
+	for key,value :=range(kvMap){
+		values=append(values,key)
+		values=append(values,value)
+	}
+
+	_, err := conn.Do("MSET", values...)
+	return err
+}
+
+func (rc *RedisClient) ClearAll() error {
+	conn:=rc.connectInit()
+	defer conn.Close()
+
+	_, err := conn.Do("FLUSHALL")
+	return err
+}
+
+func (rc *RedisClient) connectInit() redis.Conn {
+	conn:=rc.pool.Get()
+	return conn
+}
+
+func InitClient(addr string) (*RedisClient,error) {
+	pool := &redis.Pool{
+		MaxIdle: 3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func () (redis.Conn, error) {
+			c, err := redis.Dial("tcp", ":6379")
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+	return &RedisClient{pool,addr},nil
+}
---
+++ redis_test.go
@@ -0,0 +1,63 @@
+package storage
+
+import (
+	"testing"
+)
+
+func TestExists(t *testing.T) {
+	client,err:=InitClient(":6379")
+    if err!=nil {
+		t.Errorf("%s\r\n",err.Error())
+    }
+
+	client.Set("aaa",[]byte("bbb"))
+	result:=client.Exists("aaa")
+	if result!=true{
+		t.Errorf("result should be true")
+	}
+
+	result=client.Exists("bbb")
+	if result!=false{
+		t.Errorf("result should be false")
+	}
+
+	client.Lpush("aaaa","bbbb")
+	v,err:=client.Brpop("aaaa",-1)
+	value:=v.(string)
+	if value!="bbbb"{
+		t.Errorf("result should be bbbb")
+	}
+
+	client.ClearAll()
+	client.Set("aaa",[]byte("bbb"))
+	v,err=client.Get("aaa")
+	if string(v.([]byte))!="bbb"{
+		t.Errorf("result should be bbb")
+	}
+
+	client.ClearAll()
+	client.Incr("aaa")
+	v,err=client.Get("aaa")
+	if string(v.([]byte))!="1"{
+		t.Errorf("result should be 1")
+	}
+	client.ClearAll()
+
+	kvMap:=make(map[string][]byte)
+	kvMap["1"]=[]byte("123")
+	kvMap["2"]=[]byte("456")
+	client.MultiSet(kvMap)
+
+	v,err=client.Get("1")
+	if string(v.([]byte))!="123"{
+		t.Errorf("result should be 123")
+	}
+
+	v,err=client.MultiGet([]interface{}{"1","2"})
+	if string(v.([]interface{})[0].([]byte))!="123"{
+		t.Errorf("result should be 123")
+	}
+	if string(v.([]interface{})[1].([]byte))!="456"{
+		t.Errorf("result should be 456")
+	}
+}
